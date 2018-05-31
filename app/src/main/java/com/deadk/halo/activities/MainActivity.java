af025791297ce198aac73695da6595f5cb0a24f4
Huy Pham <deadknifepro@gmail.com>
2018-06-01 00:48:57 +0700

    Initial commit

---
+++ app/src/main/java/com/deadk/halo/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.deadk.halo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.deadk.halo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+    }
+}
